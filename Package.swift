6e2b6f3d982f6ee41cc60fa3ba83b60d98391ce6
Roy <shuwj81@daum.net>
2025-06-06 17:55:32 +0900

    ✨[feat]: documnet 추가

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DiContainer",
-    platforms: [.iOS(.v15), .macOS(.v12)],
+    platforms: [.iOS(.v15), .macOS(.v14)],
     products: [
         .library(
             name: "DiContainer",
             targets: ["DiContainer"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/Roy-wonji/LogMacro.git", exact: "1.0.5")
+      .package(url: "https://github.com/Roy-wonji/LogMacro.git", exact: "1.0.6"),
+      .package(url: "https://github.com/apple/swift-docc-plugin.git", exact: "1.4.4"),
     ],
     targets: [
         .target(
@@ -22,5 +23,4 @@ let package = Package(
             ]
         )
     ],
-    swiftLanguageModes: [.version("5.10.0"), .version("6.0")]
 )
