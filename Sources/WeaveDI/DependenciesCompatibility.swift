892db6cab1fa8716fbb144c1b22edfce969e8275
Roy <shuwj81@daum.net>
2025-12-26 12:42:06 +0900

    [chore]: 라이브러리 수정

--- Sources/WeaveDI/DependenciesCompatibility.swift
+++
@@ -1,9 +0,0 @@
-import Dependencies
-
-public extension DependencyValues {
-  /// Provide an unlabeled subscript to avoid ambiguous overloads in Swift 6.1.
-  subscript<Key: TestDependencyKey>(_ key: Key.Type) -> Key.Value {
-    get { self[key: key] }
-    set { self[key: key] = newValue }
-  }
-}
